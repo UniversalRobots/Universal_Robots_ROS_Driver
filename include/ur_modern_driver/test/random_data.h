1e724dcd33d57d47107be6ccc3a412fbd39d5d67
Simon Rasmussen <zetlon@gmail.com>
2017-07-09 04:03:37 +0200

    Added bitset parsing and setter for random data

--- include/ur_modern_driver/test/random_data.h
+++ include/ur_modern_driver/test/random_data.h
@@ -5,31 +5,32 @@
 #include <cstddef>
 #include <functional>
 #include <random>
+#include <bitset>
 #include "ur_modern_driver/bin_parser.h"
 
 class RandomDataTest
 {
 private:
   using random_bytes_engine = std::independent_bits_engine<std::default_random_engine, CHAR_BIT, uint8_t>;
-  uint8_t* _buf;
+  uint8_t* buf_;
   BinParser bp_;
   size_t n_;
 
 public:
-  RandomDataTest(size_t n) : _buf(new uint8_t[n]), bp_(_buf, n), n_(n)
+  RandomDataTest(size_t n) : buf_(new uint8_t[n]), bp_(buf_, n), n_(n)
   {
     random_bytes_engine rbe;
-    std::generate(_buf, _buf + n, std::ref(rbe));
+    std::generate(buf_, buf_ + n, std::ref(rbe));
   }
 
   ~RandomDataTest()
   {
-    delete _buf;
+    delete buf_;
   }
 
   BinParser getParser(bool skip = false)
   {
-    return BinParser(_buf, n_ - (skip ? sizeof(int32_t) : 0));
+    return BinParser(buf_, n_ - (skip ? sizeof(int32_t) : 0));
   }
 
   template <typename T>
@@ -39,6 +40,21 @@ public:
     bp_.parse(actual);
     return actual;
   }
+
+  template <typename T, size_t N>
+  std::bitset<N> getNext()
+  {
+    T actual;
+    bp_.parse(actual);
+    return std::bitset<N>(actual);
+  }
+
+  template <typename T>
+  void set(T data, size_t pos)
+  {
+    std::memcpy(&data, buf_+pos, sizeof(T));
+  }
+
   void skip(size_t n)
   {
     bp_.consume(n);
