fdaaacfe2c08e8a0f3ec6f296949075a04ffc081
Simon Rasmussen <zetlon@gmail.com>
2017-07-09 02:45:02 +0200

    Clean up of old driver files

--- include/ur_modern_driver/packet.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "ur_modern_driver/bin_parser.h"
-
-class Packet
-{
-public:
-  virtual bool parseWith(BinParser& bp) = 0;
-};
--- include/ur_modern_driver/parser.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "ur_modern_driver/bin_parser.h"
-#include "ur_modern_driver/packet.h"
-
-class Parser
-{
-public:
-  virtual std::unique_ptr<Packet> parse(BinParser& bp) = 0;
-};
