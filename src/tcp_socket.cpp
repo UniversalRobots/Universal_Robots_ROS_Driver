c72ec5bdedc105afc20e0984ec491a92cd3ef5fc
axelschroth <axel.schroth@isys-vision.de>
2018-05-18 13:54:45 +0200

    Fixed tcp socket: shutdown() does not destroy socket descriptor -> use close()

--- src/tcp_socket.cpp
+++ src/tcp_socket.cpp
@@ -90,7 +90,7 @@ void TCPSocket::close()
   if (state_ != SocketState::Connected)
     return;
   state_ = SocketState::Closed;
-  ::shutdown(socket_fd_, SHUT_RDWR);
+  ::close(socket_fd_);
   socket_fd_ = -1;
 }
 
