b090cdf833e8b8b51904018035a593e3b0e5f117
Felix Mauch <mauch@fzi.de>
2019-04-09 13:14:29 +0200

    added shell consumer

---
+++ include/ur_rtde_driver/comm/shell_consumer.h
@@ -0,0 +1,52 @@
+// this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
+
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+// -- END LICENSE BLOCK ------------------------------------------------
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Felix Mauch mauch@fzi.de
+ * \date    2019-04-09
+ *
+ */
+//----------------------------------------------------------------------
+
+#ifndef UR_RTDE_DRIVER_SHELL_CONSUMER_H_INCLUDED
+#define UR_RTDE_DRIVER_SHELL_CONSUMER_H_INCLUDED
+
+#include "ur_rtde_driver/comm/pipeline.h"
+#include "ur_rtde_driver/comm/package.h"
+
+namespace ur_driver
+{
+namespace comm
+{
+template <typename HeaderT>
+class ShellConsumer : public IConsumer<URPackage<HeaderT>>
+{
+public:
+  ShellConsumer() = default;
+  virtual ~ShellConsumer() = default;
+
+  virtual void setupConsumer()
+  {
+  }
+  virtual void teardownConsumer()
+  {
+  }
+  virtual void stopConsumer()
+  {
+  }
+  virtual void onTimeout()
+  {
+  }
+
+  virtual bool consume(std::shared_ptr<URPackage<HeaderT>> product);
+
+private:
+  /* data */
+};
+}  // namespace comm
+}  // namespace ur_driver
+#endif  // ifndef UR_RTDE_DRIVER_SHELL_CONSUMER_H_INCLUDED
