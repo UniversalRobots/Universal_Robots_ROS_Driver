5db802ffd05279513b0112f1fe567c397083d790
Felix Mauch <mauch@fzi.de>
2019-04-11 10:21:38 +0200

    Added executable for plain driver

---
+++ src/main_plain_driver.cpp
@@ -0,0 +1,36 @@
+// this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
+
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+// -- END LICENSE BLOCK ------------------------------------------------
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Felix Mauch mauch@fzi.de
+ * \date    2019-04-11
+ *
+ */
+//----------------------------------------------------------------------
+
+#include <ur_rtde_driver/ur/ur_driver.h>
+
+using namespace ur_driver;
+
+int main(int argc, char *argv[])
+{
+  std::string ROBOT_IP = "192.168.56.101";
+
+  if (argc > 1)
+  {
+    ROBOT_IP = argv[1];
+  }
+
+  UrDriver driver(ROBOT_IP);
+
+  while (true)
+  {
+    sleep(1);
+    // LOG_INFO("Still running");
+  }
+  return 0;
+}
---
+++ src/ur/ur_driver.cpp
@@ -0,0 +1,38 @@
+// this is for emacs file handling -*- mode: c++; indent-tabs-mode: nil -*-
+
+// -- BEGIN LICENSE BLOCK ----------------------------------------------
+// Copyright 2019 FZI Forschungszentrum Informatik
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// -- END LICENSE BLOCK ------------------------------------------------
+
+//----------------------------------------------------------------------
+/*!\file
+ *
+ * \author  Felix Mauch mauch@fzi.de
+ * \date    2019-04-11
+ *
+ */
+//----------------------------------------------------------------------
+
+#include "ur_rtde_driver/ur/ur_driver.h"
+#include <memory>
+
+namespace ur_driver{
+
+  ur_driver::UrDriver::UrDriver (const std::string& ROBOT_IP)
+  {
+    rtde_client_.reset(new rtde_interface::RTDEClient(ROBOT_IP, notifier_));
+  }
+
+}
